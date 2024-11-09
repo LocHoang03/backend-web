a9d6a370ecc5528359286187e9f1c9079cb272c8
Hoang Loc <hoangphuocloc.phurieng@gmail.com>
2024-11-09 11:01:31 +0700

    update

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.URL_BACKEND + '/api/auth/google/callback',
+      callbackURL: 'http://localhost:4000/api/auth/google/callback',
     },
     async function (accessToken, refreshToken, profile, cb) {
       let newUser, user, subscriber;
